04bc55995153eff35295493feda2cc2f20817c79
John Barker <jdabarker@gmail.com>
2021-04-18 15:46:16 +0800

    test2

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,9 +18,9 @@ function init() {
     option.value = option.textContent.toLowerCase();
   });
   const requestURL = "https://www.wikipedia.org/w/api.php?action=parse&page=Pet_door&prop=text&formatversion=2";
+  console.log("version1");
   fetch(requestURL, {method: "GET"})
   .then(response => {
-    console.log("version1");
     console.log(response);
   })
 }
